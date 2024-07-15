76f2bae3e5b5dc56d56e3d314f6005de9c67a275
osamaelanzi <575808@edu.rocmn.nl>
2024-07-15 12:54:19 +0200

    bezig met database maken

--- MakeNewAccount.php
+++ MakeNewAccount.php
@@ -1,3 +1,10 @@
 <?php
 
+function makeAccount() {
+    if (isset($_POST['aanMakenVanAccount'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        // add database connection and insert new account data here
+    }
+}
 ?>
