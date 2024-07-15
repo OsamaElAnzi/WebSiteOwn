0ddc1c4e0922f15e5e7e1c55aee386ebec6febb5
osamaelanzi <575808@edu.rocmn.nl>
2024-07-15 23:42:29 +0200

    verificatie maken

--- AccountChecker.php
+++ AccountChecker.php
@@ -1,10 +1,15 @@
 <?php
-
+include 'DBrelated.php';
 
 function acccountCheckenOfBestaat() {
     if (isset($_POST['submitCheckIfAccountExsist'])) {
         $username = $_POST['username/e-mail'];
         $password = $_POST['password'];
+        $sql = "SELECT * FROM gegevens WHERE username = '$username' AND password = '$password'";
+        $conn = conn();
+        
+        // maak login verificatie  
     }
 }
+acccountCheckenOfBestaat();
 ?>
