1f42735c89906b1f5c469eca3da89117b521fbcf
osamaelanzi <575808@edu.rocmn.nl>
2024-07-20 13:02:41 +0200

    d

--- AccountChecker.php
+++ AccountChecker.php
@@ -1,11 +1,39 @@
 <?php
 
+include 'DBrelated.php';
 
 function acccountCheckenOfBestaat() {
     if (isset($_POST['submitCheckIfAccountExsist'])) {
         $username = $_POST['username/e-mail'];
         $password = $_POST['password'];
+        
+        //controle of databse bestaat
+        $conn = conn();
+        
+        if ($conn) {
+            echo "connection successful\n";
+            $stmt = $conn->prepare("SELECT * FROM gegevens WHERE GebruikersNaam = :username AND Wachtwoord = :password");
+            $stmt->bindParam(':username', $username);
+            $stmt->bindParam(':password', $password);
+            $stmt->execute();
+            if ($stmt) {
+                echo 'check voldaan van ' . $username . ' en ' . $password . "\n";
+            } else {
+                echo 'Error: '. $stmt->errorInfo();
+            }
+            if ($stmt->rowCount() == 1) {
+                echo $stmt->rowCount();
+                header('Location: home.php?username='.$username);
+                exit();
+            } else {
+                echo 'test fout';
+            }
+        } else {
+            echo "Database connection failed.";
+        }
     }
 }
+
+acccountCheckenOfBestaat();
 // verificatie nodig
 ?>
---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="container">
+        <nav>
+            <ul>
+                <li><a link="home.php"></a></li>
+            </ul>
+        </nav>
+        <h1>Welkom <?php echo $_GET['username']; ?></h1>
+    </div>
+</body>
+</html>
